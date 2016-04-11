72f059a5293271ce7f8768f1dc7fc6d45e03fe96
aude <aude.wiki@gmail.com>
2016-04-11 16:39:46 +0200

    Add LabelCountField

---
+++ src/Fields/LabelCountField.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wikibase\Elastic\Fields;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Term\FingerprintProvider;
+
+/**
+ * @license GPL-2.0+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class LabelCountField implements Field {
+
+	/**
+	 * @see SearchIndexField::getMapping
+	 *
+	 * @return array
+	 */
+	public function getMapping() {
+		return array(
+			'type' => 'integer'
+		);
+	}
+
+	/**
+	 * @see SearchIndexField::getFieldData
+	 *
+	 * @param EntityDocument $entity
+	 *
+	 * @return int
+	 */
+	public function getFieldData( EntityDocument $entity ) {
+		if ( $entity instanceof FingerprintProvider ) {
+			return $entity->getFingerprint()->getLabels()->count();
+		}
+
+		return 0;
+	}
+
+
+}
